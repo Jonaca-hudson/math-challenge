1302800ee095adf3df171b81cb7fe2f22ed43fc8
Jonaca-hudson <31194367+Jonaca-hudson@users.noreply.github.com>
2017-08-31 12:54:09 -0400

    Update ProgramJonaca.cs

--- ProgramJonaca.cs
+++ ProgramJonaca.cs
@@ -2,101 +2,86 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-
-
-namespace mathChallenge
-
+namespace Redo
 {
-	class Program
-	{
-		 int Compare(int num1, int num2) //main function that shifts and adds each number, returns 0 for false and 1 for true
-		{
-            int compareTo = (num1 % 10) + (num2 % 10);
-			num1 = num1 / 10;
-            num2 = num2 / 10;
-            while (num1 > 0 && num2 > 0)
-                while (length != 3 || variable.Length = 3) 
+    class MainClass
+    {
+        public static void Main(string[] args)
 
-			{
-				int temp = (num1 % 10) + (num2 % 10);
+        {
+           //Ask the user for a 3 digit number
+            Console.WriteLine("Please input a three digit number...");
 
-				if (compareTo != temp)
-					return 0;
+            string input1 = Console.ReadLine();
+            int.Parse (input1);
 
-				num1 = num1 / 10;
-                num2 = num2 / 10;
-			}
-            return 1;
+            int num = int.Parse(input1);
+
+            //Ask user for second 3 digit number
+			Console.WriteLine("Please input a second three digit number...");
+
+			string input2 = Console.ReadLine();
+			int.Parse(input2);
+
+            int num2 = int.Parse(input2);
+
+            //Checks if both inputs are the same amount of digits
+            Console.WriteLine(input1.Length);
+            Console.WriteLine(input2.Length);
+
+            if (input1.Length == input2.Length)
+            {
+                Console.WriteLine("Input 1 & 2 match.");
+
+
+                //Establishes the length restriction
+                int i = 0;
+                int[] mods = new int[3];
+
+                while (i < input1.Length) 
+                
+                {
+                    //checks the inputs to make sure they add up to each other
+                    int mod1 = num % 10;
+                    int mod2 = num2 % 10;
+
+                    num /= 10;
+                    num2 /= 10;
+
+					mods[i] = mod1 + mod2;
+
+					Console.WriteLine(mod1 + mod2);
+                    i++;
+                }
+
+                //prints true or false
+                int total = mods[0];
+                bool b = true;
+                int j = 0;
+                while (j < mods.Length)
+                {
+                    int k = mods[j];
+                    if (k == total)
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        b = false;
+                    }
+                    j++;
+
+                }
+                Console.WriteLine(b);
+            }
+            else
+            {
+                //if the inputs don't add up, error message prints
+                Console.WriteLine("Input 1 & 2 do not match, try again.");
+            }
+
+
 		}
-
-		public void Main(string[] args)
-		{
-			//request the first number from the user 
-			int num1;
-			Console.WriteLine("Type any three-digit number below...");
-			String Result1 = Console.ReadLine();
-
-
-			while (!Int32.TryParse(Result1, out num1) || num1 <= -1 || num1 > 1000 || num1 < 100) //integer validation
-			{
-				Console.WriteLine("Not a valid number, Please try again...");
-				Result1 = Console.ReadLine();
-			}
-            num1 = Convert.ToInt32(Result1);
-
-            //request the second number from the user
-            int num2;
-			String Result2 = Console.ReadLine();
-			while (!Int32.TryParse(Result2, out num2) || num2 < -1 || num2 > 1000 || num1 < 100)
-
-			{
-				Console.WriteLine("Not a valid number, Please try again...");
-				Result2 = Console.ReadLine();
-			}
-            num2 = Convert.ToInt32(Result2);
-
-            // assigns each digit a variable
-            int dig1num1, dig2num1, dig3num1 = 0;
-
-			int dig1num2, dig2num2, dig3num2 = 0;
-
-			//Writing the new integers to the console
-			Console.WriteLine("Press enter to get individual digits for your first number");
-			Console.WriteLine("{0}->{1}, {2}, {3}", num1, dig3num1, dig2num1, dig1num1);
-			//Console.ReadKey();
-
-			Console.WriteLine("Press enter to get individual digits for your second number");
-			Console.WriteLine("{0}->{1}, {2}, {3}", num2, dig3num2, dig2num2, dig1num2);
-			//Console.ReadKey();
-
-			// constructing comparison aka addition variables
-			int total1, total2, total3 = 0;
-			
-
-			//combining corresponding numbers
-			total1 = (dig1num2 + dig1num2);
-			total2 = (dig2num1 + dig2num2);
-			total3 = (dig3num1 + dig3num2);
-
-			//boolean condition to test whether the corresponding numbers are equal
-			bool a = (total1 == total2 && total2 == total3);
-			bool b = (total1 != total2 && total2 == total3);
-			bool c = (total1 == total2 && total2 != total3);
-
-
-			if (a)
-			{
-				Console.WriteLine("True");
-				Console.WriteLine(a);
-				Console.ReadLine();
-				do
-				{
-					Console.WriteLine("play again? [Yes | No]");
-				} while (Console.ReadLine() != "No!");
-
-			}
-		}
-	}
+    }
 }
