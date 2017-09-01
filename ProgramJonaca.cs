418904437cb7c3bf8aa196738fa2462c4dad6eb3
Jonaca-hudson <31194367+Jonaca-hudson@users.noreply.github.com>
2017-09-01 12:31:56 -0400

    Update ProgramJonaca.cs

--- ProgramJonaca.cs
+++ ProgramJonaca.cs
@@ -10,19 +10,27 @@ namespace Redo
         public static void Main(string[] args)
 
         {
-           //Ask the user for a 3 digit number
-            Console.WriteLine("Please input a three digit number...");
+            char doAgain;
+            bool decision = true;
+
+            while(decision==true)
+            {
+
+
+                //Ask the user for a 3 digit number
+                Console.WriteLine("Please input a three digit number...");
 
             string input1 = Console.ReadLine();
-            int.Parse (input1);
+            int.Parse(input1);
+
 
             int num = int.Parse(input1);
 
             //Ask user for second 3 digit number
-			Console.WriteLine("Please input a second three digit number...");
+            Console.WriteLine("Please input a second three digit number...");
 
-			string input2 = Console.ReadLine();
-			int.Parse(input2);
+            string input2 = Console.ReadLine();
+            int.Parse(input2);
 
             int num2 = int.Parse(input2);
 
@@ -39,8 +47,8 @@ namespace Redo
                 int i = 0;
                 int[] mods = new int[3];
 
-                while (i < input1.Length) 
-                
+                while (i < input1.Length)
+
                 {
                     //checks the inputs to make sure they add up to each other
                     int mod1 = num % 10;
@@ -49,14 +57,14 @@ namespace Redo
                     num /= 10;
                     num2 /= 10;
 
-					mods[i] = mod1 + mod2;
+                    mods[i] = mod1 + mod2;
 
-					Console.WriteLine(mod1 + mod2);
+                    Console.WriteLine(mod1 + mod2);
                     i++;
                 }
 
                 //prints true or false
-                int total = mods[2];
+                int total = mods[1];
                 bool b = true;
                 int j = 0;
                 while (j < mods.Length)
@@ -71,7 +79,7 @@ namespace Redo
                         b = false;
                         //Console.WriteLine(b);
                     }
-                  
+
                     j++;
                 }
                 Console.WriteLine(b);
@@ -81,8 +89,16 @@ namespace Redo
                 //if the inputs don't add up, error message prints
                 Console.WriteLine("Input 1 & 2 do not match, try again.");
             }
+                Console.WriteLine("Would you like to run the program again? Y or N");
+                doAgain = Convert.ToChar(Console.ReadLine());
 
+                if(doAgain !='y' && doAgain != 'Y')
+                {
+                    decision = false;
+                }
+                    
 
-		}
+            }
+        }
     }
 }
